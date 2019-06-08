5942c5ea4e7ca35e3289470a9dfbb26f6e505ccf
Luismi <luismiguel.vazquez@the-cocktail.com>
2019-06-08 17:33:41 +0200

    Clean code

--- main.rb
+++ main.rb
@@ -1,15 +1,12 @@
-require './lib/data_service.rb'
 require './lib/csv_processor_service.rb'
-require './modules/file_name.rb'
 require './modules/meditions.rb'
 require 'pry'
 require 'benchmark/ips'
 
-include FileName
 include Meditions
 
 files = Dir["./datas/*.csv"]
-binding.pry
+
 structure = {}
 
 files.each do |file|
